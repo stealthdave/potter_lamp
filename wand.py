6ced608fb9c7d1ac1eec2e0ab17d02138ffd1d19
David Still <dave@stilldesigning.com>
2021-03-28 19:58:25 -0700

    Casting spells from wand works... kinda.

--- wand.py
+++ wand.py
@@ -86,88 +86,83 @@ def StartCamera():
         cv2.namedWindow("Raspberry Potter")
     # Initialize camera
     try:
-        cam = picamera.PiCamera()
+        cam = cv2.VideoCapture(0)
+        cam.set(3, 640)
+        cam.set(4, 480)
     except Exception as camera:
         print('Camera already open')
-    cam.resolution = (640, 480)
-    cam.framerate = 24
-    try:
-        while LampState():
-            FindNewPoints()
-    finally:
-        End()
+    # try:
+    #     while LampState():
+    #         FindNewPoints()
+    # finally:
+    #     End()
 
 
-def IsGesture(newA, newB, oldA, oldB, i):
+def IsGesture(a,b,c,d,i):
     """
     Determines if the point has moved.
     """
-    global ig
+
     print("point: %s" % i)
     #look for basic movements - TODO: trained gestures
-    if ((newA<(oldA-5))&(abs(newB-oldB)<1)):
+    if ((a<(c-5))&(abs(b-d)<2)):
         ig[i].append("left")
-    elif ((oldA<(newA-5))&(abs(newB-oldB)<1)):
+    elif ((c<(a-5))&(abs(b-d)<2)):
         ig[i].append("right")
-    elif ((newB<(oldB-5))&(abs(newA-oldA)<5)):
+    elif ((b<(d-5))&(abs(a-c)<5)):
         ig[i].append("up")
-    elif ((oldB<(newB-5))&(abs(newA-oldA)<5)):
+    elif ((d<(b-5))&(abs(a-c)<5)):
         ig[i].append("down")
+    #these are for moving diagnally
+	# #Angle Down Left = ADL
+    # elif (((b-d)/(c-a))>0.9):
+	#     ig[i].append("ADL")
+	# #Angle Down Right = ADR
+    # elif (((b-d)/(a-c))>0.9):
+	#     ig[i].append("ADR")
+	# #Angle Up Right = AUR
+    # elif (((d-b)/(a-c))>0.9):
+	#     ig[i].append("AUR")
+    # PART 5B 
     #check for gesture patterns in array
     astr = ''.join(map(str, ig[i]))
-    print(f'Spell string: {astr}')
     if "rightup" in astr:
         cast_spell('lumos')
     elif "rightdown" in astr:
         cast_spell('nox')
-    elif "leftdown" in astr:
-        cast_spell('colovaria')
     elif "leftup" in astr:
         cast_spell('incendio')
+    elif "leftdown" in astr:
+        cast_spell('colovaria')
+    print(f'Spell string: {astr}')
 
 
-def FindNewPoints():
+def FindWand():
     """
     FindWand is called to find all potential wands in a scene.  These are then 
     tracked as points for movement.  The scene is reset every 3 seconds.
     """
-    global old_frame, \
-           old_gray, \
-           p0, \
-           mask, \
-           color, \
-           ig, \
-           img, \
-           frame, \
-           cam
+    global rval,old_frame,old_gray,p0,mask,color,ig,img,frame
     try:
-        while LampState():
-            try:
-                old_frame = cam.capture(stream, format='jpeg')
-            except:
-                print("resetting points")
-            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-            old_frame = cv2.imdecode(data, 1)
-            cv2.flip(old_frame,1,old_frame)
-            old_gray = cv2.cvtColor(old_frame,cv2.COLOR_BGR2GRAY)
-
-            #TODO: trained image recognition
-            p0 = cv2.HoughCircles(old_gray,
-                                cv2.HOUGH_GRADIENT,
-                                3,
-                                100,
-                                param1=100,
-                                param2=30,
-                                minRadius=4,
-                                maxRadius=15)
-            if p0 is not None:
-                p0.shape = (p0.shape[1], 1, p0.shape[2])
-                p0 = p0[:,:,0:2]
-                mask = np.zeros_like(old_frame)
-                ig = [[0] for x in range(20)]
-                print("Run TrackWand")
-                TrackWand()
-
+        rval, old_frame = cam.read()
+        cv2.flip(old_frame,1,old_frame)
+        old_gray = cv2.cvtColor(old_frame,cv2.COLOR_BGR2GRAY)
+        cv2.equalizeHist(old_gray)
+        old_gray = cv2.GaussianBlur(old_gray,(9,9),1.5)
+        dilate_kernel = np.ones(dilation_params, np.uint8)
+        old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        old_gray = clahe.apply(old_gray)
+        #TODO: trained image recognition
+        p0 = cv2.HoughCircles(old_gray,cv2.HOUGH_GRADIENT,3,50,param1=240,param2=8,minRadius=4,maxRadius=15)
+        if p0 is not None:
+            p0.shape = (p0.shape[1], 1, p0.shape[2])
+            p0 = p0[:,:,0:2]
+            mask = np.zeros_like(old_frame)
+            ig = [[0] for x in range(20)]
+        if LampState():
+            print("finding...")
+            threading.Timer(3, FindWand).start()
     except Exception as e:
         print(f'Error: {e}')
         End()
@@ -178,106 +173,83 @@ def TrackWand():
     """
     Tracks wand points for 3 seconds.
     """
-    global old_frame, \
-           old_gray, \
-           p0, \
-           mask, \
-           color, \
-           ig, \
-           img, \
-           frame
-    color = (0,0,255)
+    global rval,old_frame,old_gray,p0,mask,color,ig,img,frame
     try:
-        old_frame = cam.capture(stream, format='jpeg')
-    except:
-        print("resetting points")
-    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-    old_frame = cv2.imdecode(data, 1)
-    cv2.flip(old_frame,1,old_frame)
-    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    # Take first frame and find circles in it
-    p0 = cv2.HoughCircles(old_gray,
-                          cv2.HOUGH_GRADIENT,
-                          3,
-                          100,
-                          param1=100,
-                          param2=30,
-                          minRadius=4,
-                          maxRadius=15)
-    try:
-        p0.shape = (p0.shape[1], 1, p0.shape[2])
-        p0 = p0[:,:,0:2]
-    except:
+        color = (0,0,255)
+        rval, old_frame = cam.read()
+        cv2.flip(old_frame,1,old_frame)
+        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+        cv2.equalizeHist(old_gray)
+        old_gray = GaussianBlur(old_gray,(9,9),1.5)
+        dilate_kernel = np.ones(dilation_params, np.uint8)
+        old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        old_gray = clahe.apply(old_gray)
+
+        # Take first frame and find circles in it
+        p0 = cv2.HoughCircles(old_gray,cv2.HOUGH_GRADIENT,3,50,param1=240,param2=8,minRadius=4,maxRadius=15)
+        if p0 is not None:
+            p0.shape = (p0.shape[1], 1, p0.shape[2])
+            p0 = p0[:,:,0:2]
+            mask = np.zeros_like(old_frame)
+    except Exception as e:
         print("No points found")
-	# Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)
-
-    timer = time.time()
-
-    # Run for 3 seconds and reset
-    while LampState() and (time.time() - timer) < 3:
-        frame = cam.capture(stream, format='jpeg')
-        data2 = np.fromstring(stream.getvalue(), dtype=np.uint8)
-        frame = cv2.imdecode(data2, 1)
-        cv2.flip(frame,1,frame)
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Create a mask image for drawing purposes
+    while LampState():
         try:
-            # calculate optical flow
-            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-            # Select good points
-            good_new = p1[st==1]
-            good_old = p0[st==1]
-            print('start looking for points')
-            # draw the tracks
-            for i,(new,old) in enumerate(zip(good_new,good_old)):
-                newA, newB = new.ravel()
-                oldA, oldB = old.ravel()
-                # only try to detect gesture on highly-rated points (below 15)
-                print('is there a gesture?')
-                if (i<15):
-                    print('yes!')
-                    IsGesture(newA, newB, oldA, oldB, i)
-                    dist = math.hypot(newA - oldA, newB - oldB)
-                    print(f'dist: {dist}')
+            rval, frame = cam.read()
+            cv2.flip(frame,1,frame)
+            if p0 is not None:
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.equalizeHist(frame_gray)
+                frame_gray = cv2.GaussianBlur(frame_gray,(9,9),1.5)
+                dilate_kernel = np.ones(dilation_params, np.uint8)
+                frame_gray = cv2.dilate(frame_gray, dilate_kernel, iterations=1)
+                frame_clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+                frame_gray = frame_clahe.apply(frame_gray)
+
+                # calculate optical flow
+                p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+                # Select good points
+                good_new = p1[st==1]
+                good_old = p0[st==1]
+
+                # draw the tracks
+                for i,(new,old) in enumerate(zip(good_new,good_old)):
+                    a,b = new.ravel()
+                    c,d = old.ravel()
+                    # only try to detect gesture on highly-rated points (below 10)
+                    if (i<15):
+                        IsGesture(a,b,c,d,i)
+                    dist = math.hypot(a - c, b - d)
                     if (dist<movment_threshold):
-                        cv2.line(mask, (newA, newB),(oldA, oldB),(0,255,0), 2)
-                        cv2.circle(frame,(newA, newB),5,color,-1)
-                        cv2.putText(frame,
-                                    str(i),
-                                    (newA, newB),
-                                    cv2.FONT_HERSHEY_SIMPLEX,
-                                    1.0,
-                                    (0,0,255))
+                        cv2.line(mask, (a,b),(c,d),(0,255,0), 2)
+                    cv2.circle(frame,(a,b),5,color,-1)
+                    if debug_opencv:
+                        cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255)) 
+                img = cv2.add(frame,mask)
+                # save for debug
+                print('Save test image')
+                cv2.imwrite('test.jpg', img)
+                if debug_opencv:
+                    cv2.putText(img, "Press ESC to close.", (5, 25),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+
+            if debug_opencv:
+                cv2.imshow("Raspberry Potter", frame)
+
+            # get next frame
+            rval, frame = cam.read()
+
+            # Now update the previous frame and previous points
+            old_gray = frame_gray.copy()
+            p0 = good_new.reshape(-1,1,2)
         except IndexError:
-            print("Index error")
-            End()
-            break
-        except:
+            print("Index error - Tracking")  
+        except Exception as error:
             e = sys.exc_info()[0]
-            print(f'TrackWand Error: {e}')
-            # End()
-            break
-        img = cv2.add(frame,mask)
-        # save for debug
-        cv2.imwrite('test.jpg', frame_gray)
-
-        if debug_opencv:
-            cv2.putText(img,
-                        "Press ESC to close.",
-                        (5, 25),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1.0,
-                        (255,255,255))
-            cv2.imshow("Raspberry Potter", frame)
-
-        # get next frame
-        frame = cam.capture(stream, format='jpeg')
-        data3 = np.fromstring(stream.getvalue(), dtype=np.uint8)
-        frame = cv2.imdecode(data3, 1)
-
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0 = good_new.reshape(-1,1,2)
+            print("Tracking Error: %s" % e)
 
 
 def CaptureStill():
@@ -326,9 +298,10 @@ def WatchSpellsOn():
     lumos(0)
     # start capturing
     StartCamera()
+    FindWand()
     # track wand
-    # print("Begin Tracking Wand")
-    # TrackMotion()
+    print("Begin Tracking Wand")
+    TrackWand()
 
 def WatchSpellsOff():
     """Stop watching for spells."""
